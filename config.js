eebafd0f579acc1b4b8e016220aa0a63d01dae29
LanThoi <86434904+LanThoi@users.noreply.github.com>
2021-06-25 02:54:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     btnYes: 'EM ĐỒNG Ý',
     btnNo: 'DẠ CÓ Ạ',
     question:'Trên thế giới hơn 7 tỉ người mà sao vợ lại yêu chồng <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho chồng <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'chồng biết mà 🥰. Yêu vợ nhiều nhiều 😘😘',
+    messDesc: 'Nhớ vợ quá à.',
+    btnAccept: 
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
